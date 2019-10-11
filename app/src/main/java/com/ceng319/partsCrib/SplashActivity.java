850282dd696fe1aae866e0c71929177956b57ab0
asperham <rx-78-2@live.ca>
2019-10-11 15:28:43 -0400

    add

---
+++ app/src/main/java/com/ceng319/partsCrib/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.ceng319.partsCrib;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
